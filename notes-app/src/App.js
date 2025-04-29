806679af9a3fb151d0acd8cf6f399cfad5d5f131
Ramiro Zheng <ramazheng@gmail.com>
2025-04-29 14:30:03 -0700

    change

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,24 +1,15 @@
+import{ BrowserRouter as Router, Route, Router, Routes} from 'react-router-dom'
+import Home from '/Home';
+import Header from './Header';
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  return(
+    <div className='app'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<><Header/><Home/></>}/>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
