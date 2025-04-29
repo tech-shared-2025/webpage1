b0fe7a3ec06b2821c086e2c85e67b4f06b99e847
Ramiro Zheng <ramazheng@gmail.com>
2025-04-29 14:08:40 -0700

    home

---
+++ notes-app/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '.../css/home.css';
+export default function Home(){
+    return (
+        <div className='home'>
+            <div className='content'>
+                <img src='Notes.png' alt='illustraction' className='illustraction'/>
+                <h5>create your note</h5>
+            </div>
+        </div>
+    );
+}
